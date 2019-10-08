5cf11e5d5d4151ac2efe4c40ab06bf0e38f4ed11
Manju <manjunatha.s@egovernments.org>
2019-10-08 14:37:43 +0530

    changed to dockerhub

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('manju4d90/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('manju4d90')
+            registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
